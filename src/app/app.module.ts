4a51f676595031f05d24b4d7a836222a9f943e0a
Nikola Madic <nikola.madic97@gmail.com>
2020-11-18 11:26:32 +0100

    Ingredients loaded through the initial state, AddIngredient shopping-list action and reducers added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import {HttpClientModule} from '@angular/common/http';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
 import {CoreModule} from './core/core.module';
+import {StoreModule} from '@ngrx/store';
+import {shoppingListReducer} from './shopping-list/store/shopping-list.reducers';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import {CoreModule} from './core/core.module';
     HttpClientModule,
     ShoppingListModule,
     AuthModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
   ],
   bootstrap: [AppComponent],
 })
